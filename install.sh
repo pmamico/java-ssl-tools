434cec64160548a16e301b10f2b45b8b70a457ae
Mico Papp <miklos.papp@guidance.hu>
2022-05-23 10:55:54 +0200

    windows support

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sudo curl -o /usr/local/bin/sslping -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslping && sudo chmod +x /usr/local/bin/sslping
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # 32 bits Windows NT platform
-    sudo curl -o /usr/local/bin/sslinstall -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslinstall && sudo chmod +x /usr/local/bin/sslinstall
-    sudo curl -o /usr/local/bin/sslping -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslping && sudo chmod +x /usr/local/bin/sslping
+    curl -o $HOME/bin/sslinstall -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslinstall
+    curl -o $HOME/bin/sslping -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslping
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     # 64 bits Windows NT platform
-    sudo curl -o /usr/local/bin/sslinstall -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslinstall && sudo chmod +x /usr/local/bin/sslinstall
-    sudo curl -o /usr/local/bin/sslping -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslping && sudo chmod +x /usr/local/bin/sslping
+    curl -o $HOME/bin/sslinstall -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslinstall
+    curl -o $HOME/bin/sslping -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslping
 fi
 
 echo "sslinstall and sslping are successfully installed!"
