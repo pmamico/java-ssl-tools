2b2f0844d2310f43e697434f1e02642f5ba007fe
Mico Papp <miklos.papp@guidance.hu>
2022-05-23 10:11:24 +0200

    added install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Mac OS X platform
+    sudo curl -o /usr/local/bin/sslinstall -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslinstall && sudo chmod +x /usr/local/bin/sslinstall
+    sudo curl -o /usr/local/bin/sslping -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslping && sudo chmod +x /usr/local/bin/sslping
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # GNU/Linux platform
+    sudo curl -o /usr/local/bin/sslinstall -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslinstall && sudo chmod +x /usr/local/bin/sslinstall
+    sudo curl -o /usr/local/bin/sslping -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslping && sudo chmod +x /usr/local/bin/sslping
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # 32 bits Windows NT platform
+    sudo curl -o /usr/local/bin/sslinstall -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslinstall && sudo chmod +x /usr/local/bin/sslinstall
+    sudo curl -o /usr/local/bin/sslping -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslping && sudo chmod +x /usr/local/bin/sslping
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # 64 bits Windows NT platform
+    sudo curl -o /usr/local/bin/sslinstall -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslinstall && sudo chmod +x /usr/local/bin/sslinstall
+    sudo curl -o /usr/local/bin/sslping -s https://raw.githubusercontent.com/pmamico/java-ssl/main/sslping && sudo chmod +x /usr/local/bin/sslping
+fi
+
+echo "sslinstall and sslping are successfully installed!"
