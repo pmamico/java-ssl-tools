146d19e129c77c38987c0b0943ef4a84f3aee057
Mico Papp <miklos.papp@guidance.hu>
2022-07-01 09:33:05 +0200

    uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -4,22 +4,29 @@ if [ "$(uname)" == "Darwin" ]; then
     # Mac OS X platform
     SSLI=/usr/local/bin/sslinstall
     SSLP=/usr/local/bin/sslping
+    sudo rm $SSLI
+    sudo rm $SSLP
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # GNU/Linux platform
     SSLI=/usr/local/bin/sslinstall
     SSLP=/usr/local/bin/sslping
+    sudo rm $SSLI
+    sudo rm $SSLP
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # 32 bits Windows NT platform
     SSLI=$HOME/bin/sslinstall
     SSLP=$HOME/bin/sslping
+    rm $SSLI
+    rm $SSLP
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     # 64 bits Windows NT platform
     SSLI=$HOME/bin/sslinstall
     SSLP=$HOME/bin/sslping
+    rm $SSLI
+    rm $SSLP
 fi
 
-rm $SSLI
-rm $SSLP
+
 
 if test -f "$SSLI"; then
     echo "Failed to remove sslinstall. Please remove it manually at $SSLI"
