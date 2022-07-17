a2bdab76b7c248b8f97bf7e51606a959bdc86cee
Mico Papp <miklos.papp@guidance.hu>
2022-07-17 20:45:35 +0200

    shellcheck action added

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-![Cert](.img/ssl.svg)  
-![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white)  
+![shellcheck](https://github.com/pmamico/java-ssl-tools/actions/workflows/shellcheck.yml/badge.svg)  
+
+![Cert](.img/ssl.svg)    
 ![Windows](https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white)
 ![Linux](https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black)
 ![Mac OS](https://img.shields.io/badge/mac%20os-000000?style=for-the-badge&logo=macos&logoColor=F0F0F0)  
@@ -9,7 +10,7 @@
 A keytool alternative with handshake checking mechanism.  
 Install certificate and check the connection like:
 
-![demo](jssl.gif)
+![demo](.doc/jssl.gif)
 
 
 
--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@
 uname="$(uname)"
 if [ "$(uname)" == "Darwin" ]; then
     # Mac OS X platform
-    sudo curl -o /usr/local/bin/jssl -s https://raw.githubusercontent.com/pmamico/java-ssl-tools/main/jssl && sudo chmod +x /usr/local/bin/jssl
+    sudo curl -o /usr/local/bin/jssl -s https://raw.githubusercontent.com/pmamico/java-ssl-tools/main/src/jssl && sudo chmod +x /usr/local/bin/jssl
     INSTALL_PATH=/usr/local/bin/jssl
 elif [ "${uname:0:5}" == "Linux" ]; then
     # GNU/Linux platform
-     sudo curl -o /usr/local/bin/jssl -s https://raw.githubusercontent.com/pmamico/java-ssl-tools/main/jssl && sudo chmod +x /usr/local/bin/jssl
+     sudo curl -o /usr/local/bin/jssl -s https://raw.githubusercontent.com/pmamico/java-ssl-tools/main/src/jssl && sudo chmod +x /usr/local/bin/jssl
      INSTALL_PATH=/usr/local/bin/jssl
 elif [ "${uname:0:10}" == "MINGW32_NT" ]; then
     # 32 bits Windows NT platform
-    curl -o "$HOME"/bin/jssl -s https://raw.githubusercontent.com/pmamico/java-ssl-tools/main/jssl
+    curl -o "$HOME"/bin/jssl -s https://raw.githubusercontent.com/pmamico/java-ssl-tools/main/src/jssl
     INSTALL_PATH=$HOME/bin/jssl
 elif [ "${uname:0:10}" == "MINGW64_NT" ]; then
     # 64 bits Windows NT platform
-   curl -o "$HOME"/bin/jssl -s https://raw.githubusercontent.com/pmamico/java-ssl-tools/main/jssl
+   curl -o "$HOME"/bin/jssl -s https://raw.githubusercontent.com/pmamico/java-ssl-tools/main/src/jssl
    INSTALL_PATH=$HOME/bin/jssl
 fi
 
