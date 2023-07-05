025f36709c591c7070348398747d6a8b1081d22f
Mico Papp <miklos.papp@guidance.hu>
2023-07-05 16:10:58 +0200

    windows install fix

--- install.sh
+++ install.sh
@@ -11,10 +11,12 @@ elif [ "${uname:0:5}" == "Linux" ]; then
      INSTALL_PATH=/usr/local/bin/jssl
 elif [ "${uname:0:10}" == "MINGW32_NT" ]; then
     # 32 bits Windows NT platform
+    mkdir -p "$HOME"/bin
     curl -o "$HOME"/bin/jssl -s https://raw.githubusercontent.com/pmamico/java-ssl-tools/main/src/jssl
     INSTALL_PATH=$HOME/bin/jssl
 elif [ "${uname:0:10}" == "MINGW64_NT" ]; then
     # 64 bits Windows NT platform
+   mkdir -p "$HOME"/bin
    curl -o "$HOME"/bin/jssl -s https://raw.githubusercontent.com/pmamico/java-ssl-tools/main/src/jssl
    INSTALL_PATH=$HOME/bin/jssl
 fi
